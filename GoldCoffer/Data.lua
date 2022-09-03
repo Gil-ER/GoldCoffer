615b0728fea40fe061c3a30bad12fe146ebdf6e9
Gil-ER <90463047+Gil-ER@users.noreply.github.com>
2022-09-03 16:46:13 -0400

    Add files via upload

---
+++ GoldCoffer/Data.lua
@@ -0,0 +1,117 @@
+
+local addon, ns = ...
+
+function ns:updateGold()
+	-- initializes table structure if none exists
+	
+	-- updates this toons gold
+	ns.player = UnitName("player");
+	ns.srv = GetRealmName();
+	GoldCoffer = GoldCoffer or {};
+	GoldCoffer.Servers = GoldCoffer.Servers or {};
+	GoldCoffer.Servers[ns.srv] = GoldCoffer.Servers[ns.srv] or {};
+	GoldCoffer.Servers[ns.srv][ns.player] = GetMoney();
+	
+	--Date history data
+	local curTime = time();
+	local curGold = ns:GetTotalGold(false);
+	local resetDay = curTime + C_DateAndTime.GetSecondsUntilDailyReset()
+	local resetWeek = curTime + C_DateAndTime.GetSecondsUntilWeeklyReset()
+	
+	--Initialize tables in a new database
+	GoldCoffer.History = GoldCoffer.History or {};
+	GoldCoffer.History.Today = GoldCoffer.History.Today or curGold;
+	GoldCoffer.History.Yesterday = GoldCoffer.History.Yesterday or curGold;
+	GoldCoffer.History.LastWeek = GoldCoffer.History.LastWeek or curGold;
+	GoldCoffer.History.Resets = GoldCoffer.History.Resets or {};
+	GoldCoffer.History.Resets.Day = GoldCoffer.History.Resets.Day or resetDay;
+	GoldCoffer.History.Resets.Week = GoldCoffer.History.Resets.Week or resetWeek;
+	
+	--Check to see if we have passed a daily reset and advance data if we have
+	if GoldCoffer.History.Resets.Day < resetDay then
+		GoldCoffer.History.Yesterday = GoldCoffer.History.Today;
+		GoldCoffer.History.Today = curGold;		
+		GoldCoffer.History.Resets.Day = resetDay;
+	end;
+
+	--Check to see if we have passed a weekly reset and advance data if we have
+	if GoldCoffer.History.Resets.Week < resetWeek then		
+		GoldCoffer.History.LastWeek = GoldCoffer.History.Today;		
+		GoldCoffer.History.Resets.Week = resetWeek;
+	end	
+
+--[[
+		["Today"] = Total Gold on first login of the day,
+		["Yesterday"] = Total Gold on last login of the last day you were online,
+		["LastWeek"] = Total Gold on last login prior to weekly reset,
+		["Resets"] = {
+			["Day"] = ###,
+			["Week"] = ##,
+		}
+]]
+end;
+
+function ns:GetServers()
+	--returns a table containing all servers in the data table sorted alphabetically 
+	local s = {};
+	for k, v in pairs (GoldCoffer.Servers) do
+		table.insert(s, k);
+	end;
+	table.sort(s);
+	return s;	
+end;
+
+function ns:GetTotalGold(iconFlag)
+	--iconFlag:	- true return is formated with icons for gold silver copper
+	--			- false of nil returns total in copper
+	local tg = 0;
+	local s = ns:GetServers();
+	for k, v in pairs(GoldCoffer.Servers) do
+		for t, g in pairs(GoldCoffer.Servers[k]) do
+			tg = tg + g;
+		end; 	--in pairs t,g
+	end;	--in pairs k,v
+	if iconFlag then tg = ns:GoldSilverCopper(tg); end;
+	return tg;
+end;
+
+function ns:GetServerGold(s, iconFlag)
+	--iconFlag:	- true return is formated with icons for gold silver copper
+	--			- false of nil returns total in copper
+	local sg = 0;
+	for t, g in pairs(GoldCoffer.Servers[s]) do
+		sg = sg + g;
+	end; 	--in pairs t,g	
+	if iconFlag then sg = ns:GoldSilverCopper(sg); end;
+	return sg;
+end;
+
+function ns:GetTodaysChange()
+	local curGold = ns:GetTotalGold(false);
+	local diff = curGold - GoldCoffer.History.Today or curGold;
+	if diff < 0 then
+		return ns:colorString("red", ns:GoldSilverCopper(diff));
+	else
+		return ns:colorString("green", ns:GoldSilverCopper(diff));
+	end;
+end;
+
+function ns:GetYesterdaysChange()
+	local curGold = ns:GetTotalGold(false);
+	local diff = curGold - GoldCoffer.History.Yesterday or curGold;
+	if diff < 0 then
+		return ns:colorString("red", ns:GoldSilverCopper(diff));
+	else
+		return ns:colorString("green", ns:GoldSilverCopper(diff));
+	end;
+end;
+
+function ns:GetWeeksChange()
+	local curGold = ns:GetTotalGold(false);
+	local diff = curGold - GoldCoffer.History.LastWeek or curGold;
+	if diff < 0 then
+		return ns:colorString("red", ns:GoldSilverCopper(diff));
+	else
+		return ns:colorString("green", ns:GoldSilverCopper(diff));
+	end;	
+end;
